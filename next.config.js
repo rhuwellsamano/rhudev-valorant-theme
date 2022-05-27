8435a9ae64bf3d59580fb84308abacd1d04ead5c
Rhuwell Samano <20153521+rhuwellsamano@users.noreply.github.com>
2022-05-27 16:24:18 -0400

    added React, Next, Tailwind, and Sanity

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const STUDIO_REWRITE = {
+  source: "/studio/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+};
+
+module.exports = {
+  rewrites: () => [STUDIO_REWRITE],
+};
