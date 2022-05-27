41ae47e40bb331132fd1ebf6acbfa31ae11f9298
Rhuwell Samano <20153521+rhuwellsamano@users.noreply.github.com>
2022-05-27 18:46:22 -0400

    started work on HeroHeader but the CSS is janky. The video is filling up the container height made by the H1 tag next to it.. so it's filling up but I can't seem to get the body container to fill the screen atm..

--- components/Footer.js
+++ components/Footer.js
@@ -81,7 +81,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:mt-0 md:order-1">
           <p className="text-center text-base text-gray-400">
-            &copy; 2020 Workflow, Inc. All rights reserved.
+            &copy; 2022 Knave Media Creative Agency. All rights reserved.
           </p>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
+/* eslint-disable react/no-unescaped-entities */
 import Head from "next/head";
-import Image from "next/image";
-// import styles from '../styles/Home.module.css'
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+// import Image from "next/image";
+import HeroHeader from "../components/HeroHeader/HeroHeader.js";
 
 export default function Home() {
   return (
-    <h1 className="text-5xl font-bold">
-      Hello World! Rhu's Valorant Portfolio Website is WORKING!
-    </h1>
+    <section
+      className="relative 
+      px-[5%] lg:px-[7.3%]"
+    >
+      <span
+        className="absolute h-f h-full top-0 border-l
+    border-slate-900/40 z-10 
+    left-[5%] lg:left-[7.3%]
+    "
+      ></span>
+      <HeroHeader />
+      <span
+        className="absolute border-solid bottom-0 left-0 border-l-white border-t-[transparent] z-10
+          border-t-[5vw] lg:border-t-[50px]
+          border-l-[5vw] lg:border-l-[50px]"
+      ></span>
+      <h1 className="text-5xl font-bold">
+        Hello World! Rhu's Valorant Portfolio Website is WORKING! Hello World!
+        Rhu's Valorant Portfolio Website is WORKING! Hello World! Rhu's Valorant
+        Portfolio Website is WORKING! Hello World! Rhu's Valorant Portfolio
+        Website is WORKING! Hello World! Rhu's Valorant Portfolio Website is
+        WORKING! Hello World! Rhu's Valorant Portfolio Website is WORKING! Hello
+        World! Rhu's Valorant Portfolio Website is WORKING! Hello World! Rhu's
+        Valorant Portfolio Website is WORKING! Hello World! Rhu's Valorant
+        Portfolio Website is WORKING! Hello World! Rhu's Valorant Portfolio
+        Website is WORKING! Hello World! Rhu's Valorant Portfolio Website is
+        WORKING! Hello World! Rhu's Valorant Portfolio Website is WORKING!
+      </h1>
+    </section>
   );
 }
