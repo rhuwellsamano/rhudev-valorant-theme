3bb39f380ae635494ae22644a9d6d3688a4bebba
Rhuwell Samano <20153521+rhuwellsamano@users.noreply.github.com>
2022-05-27 18:57:37 -0400

    added valorant-red to colors in tailwind config

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,7 +8,7 @@ const navigation = [
 
 export default function Navbar() {
   return (
-    <header className="bg-indigo-600">
+    <header className="bg-valorant-red">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
         <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
           <div className="flex items-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "valorant-red": "#fa4454",
+      },
+    },
   },
   plugins: [],
 };
