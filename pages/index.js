578f833212291e861517811a6150a9e1d9b21374
Rhuwell Samano <20153521+rhuwellsamano@users.noreply.github.com>
2022-05-27 17:39:09 -0400

    created components folder with Navbar, Footer, and Layout components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 // import styles from '../styles/Home.module.css'
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
