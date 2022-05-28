5f92523fa2b198ac2c9954b2f9cc7f65acccacce
Rhuwell Samano <20153521+rhuwellsamano@users.noreply.github.com>
2022-05-27 21:57:37 -0400

    cleaned up HeroHeader a bit

--- components/HeroHeader/HeroHeader.js
+++ components/HeroHeader/HeroHeader.js
@@ -1,39 +1,6 @@
 import Poster from "./poster.webp";
 import Button from "../Button";
 
-// const BackgroundVideo = () => (
-//   <div className="absolute top-0 left-0 object-cover w-full h-full z-0">
-//     <video
-//       poster={Poster}
-//       autoPlay
-//       muted
-//       playsInline
-//       loop
-//       className="w-full h-full object-cover"
-//     >
-//       <source
-//         src="https://assets.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt7f806cb08ad5d99f/61d908612e109d6c649d3b87/Disruption_VAL_DESKTOP_1730x720.mp4"
-//         type="video/mp4"
-//       />
-//     </video>
-//   </div>
-// );
-
-// export default BackgroundVideo;
-
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
-import {
-  AnnotationIcon,
-  ChatAlt2Icon,
-  InboxIcon,
-  MenuIcon,
-  QuestionMarkCircleIcon,
-  XIcon,
-} from "@heroicons/react/outline";
-import { ChevronDownIcon } from "@heroicons/react/solid";
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
